235af5f4bbe428562fdb32f6862d243352f6c7b5
anewj <anewj@deakin.edu.au>
2018-09-26 00:08:23 +1000

    removed mongo dependency

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url)
-    .then(() => {
-        console.log("Successfully connected to the database");
-    }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
+// const dbConfig = require('./config/database.config.js');
+// const mongoose = require('mongoose');
+//
+// mongoose.Promise = global.Promise;
+//
+// // Connecting to the database
+// mongoose.connect(dbConfig.url)
+//     .then(() => {
+//         console.log("Successfully connected to the database");
+//     }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...');
+//     process.exit();
+// });
 
 //paths
 const py_env = process.argv.slice(2)[0];
